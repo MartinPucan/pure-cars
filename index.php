b17f4ee92d7515528314451fdda40302e767c034
Martin Pucan <martin.pucan@pilulka.cz>
2020-07-02 11:04:22 +0200

    rename template

--- index.php
+++ index.php
@@ -23,6 +23,6 @@ $request = $_SERVER['REQUEST_URI'];
 //         break;
      default:
          http_response_code(404);
-         require __DIR__ . '/templates/404.php';
+         require __DIR__ . '/templates/404.view.php';
          break;
  }
